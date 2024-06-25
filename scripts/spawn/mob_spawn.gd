52422002d0ad74b0902b8c1a9f90fcfb15c66aa4
Nenpotato <59008922+Sod3n@users.noreply.github.com>
2024-06-25 21:45:05 +0700

    change to scene

--- scripts/spawn/mob_spawn.gd
+++ scripts/spawn/mob_spawn.gd
@@ -1,15 +1,9 @@
 class_name MobSpawn
 extends Node
 
-const BASIC_MOB = preload("res://scenes/basic_mob.tscn")
-const RANGE_MOB = preload("res://scenes/range_mob.tscn")
-const FAST_MOB = preload("res://scenes/fast_mob.tscn")
-
-enum Type {BASIC, RANGER, ASSASIN}
-
 signal spawned
 
-@export var type : Type = Type.BASIC 
+@export var type : PackedScene = preload("res://scenes/basic_mob.tscn")
 @export var count : int = 1
 @export var frequency : float = 5
 @export var direction_priorities : Array[PointRandomizer.Priorities] \
@@ -36,13 +30,6 @@ func spawn_mobs():
 	for i in count:
 		var spawn_position = Global.point_randomizer.generate_random_point_for_spawn(direction_priorities)
 		var mob = null
-		match type:
-			Type.BASIC:
-				mob = BASIC_MOB.instantiate()
-			Type.RANGER:
-				mob = RANGE_MOB.instantiate()
-			Type.ASSASIN:
-				mob = FAST_MOB.instantiate()
 		
 		get_tree().root.add_child(mob)
 		mob.owner = get_tree().root
